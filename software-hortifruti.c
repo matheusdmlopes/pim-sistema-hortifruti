4c11c0dca23e5c3f98b7a498f7e48e308657430e
matheusdmlopes <matheusdemarcolopes@gmail.com>
2024-11-16 20:26:41 -0300

    CRUD dos produtos realizado no arquivo principal

--- software-hortifruti.c
+++ software-hortifruti.c
@@ -1,52 +1,276 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-typedef struct
-{
+typedef struct {
     int id;
     int quantidade;
     double preco;
     char nome[50];
-} TProduto;
+} Produto;
 
 void cadastrarProdutos()
 {
-    TProduto produto, produtos[5];
+    FILE *arquivo;
+    Produto produto;
     int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        produtos[i].id = i + 1;
-
-        printf("Digite o nome do produto: ");
-        fgets(produtos[i].nome, sizeof(produtos[i].nome), stdin);
-
-        printf("Digite a quantidade do produto: ");
-        scanf("%d", &produtos[i].quantidade);
-
-        printf("Digite o preco do produto: ");
-        scanf("%lf", &produtos[i].preco);
-
-        getchar();
-
-        printf("Seu produto foi cadastrado, informacoes abaixo: \n");
-        printf("ID: %d\n", produtos[i].id);
-        printf("Quantidade: %d\n", produtos[i].quantidade);
-        printf("Preço: %.2lfR$\n", produtos[i].preco);
-        printf("Nome: %s\n", produtos[i].nome);
+    arquivo = fopen("dados.bin", "ab");
+    if(arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        exit(1);
     }
-    printf("Produtos cadastrados:\n");
-    for (i = 0; i < 5; i++)
-    {
-        printf("ID: %d\n", produtos[i].id);
-        printf("Nome: %s", produtos[i].nome);
-        printf("Quantidade: %d\n", produtos[i].quantidade);
-        printf("Preço: %.2lfR$\n", produtos[i].preco);
+
+    printf("\n--- Cadastro de Novo Produto ---\n");
+
+    printf("Digite o ID: ");
+    scanf("%d", &produto.id);
+    getchar();
+
+    printf("Digite o nome do produto: ");
+    fgets(produto.nome, sizeof(produto.nome), stdin);
+    produto.nome[strcspn(produto.nome, "\n")] = '\0';
+
+    printf("Digite a quantidade do produto: ");
+    scanf("%d", &produto.quantidade);
+
+    printf("Digite o preco do produto: ");
+    scanf("%lf", &produto.preco);
+
+    getchar();
+
+    fwrite(&produto, sizeof(Produto), 1, arquivo);
+
+    fclose(arquivo);
+
+    printf("\n--- Registro incluído com sucesso ---\n");
+    printf("ID: %d\n", produto.id);
+    printf("Nome: %s\n", produto.nome);
+    printf("Quantidade: %d\n", produto.quantidade);
+    printf("Preco: %.2lf\n", produto.preco);
+    printf("-----------------------------------\n");
+}
+
+void consultarProdutos()
+{
+    FILE *arquivo;
+    Produto produto;
+    int id_busca, encontrou = 0;
+
+    arquivo = fopen("dados.bin", "rb");
+    if(arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        exit(1);
+    }
+
+    printf("\nDigite o ID para pesquisa: ");
+    scanf("%d", &id_busca);
+
+    while(fread(&produto, sizeof(Produto), 1, arquivo)) {
+        if(produto.id == id_busca) {
+            encontrou = 1;
+            printf("\n--- Registro encontrado ---\n");
+            printf("ID: %d\n", produto.id);
+            printf("Nome: %s\n", produto.nome);
+            printf("Quantidade: %d\n", produto.quantidade);
+            printf("Preco: %.2lf\n", produto.preco);
+            printf("---------------------------\n");
+            break;
+        }
+    }
+
+    fclose(arquivo);
+
+    if(encontrou == 0) {
+        printf("\nNao ha registro com o valor pesquisado.\n");
+    }
+}
+
+void consultarTodosProdutos () {
+    FILE *arquivo;
+    Produto produto;
+
+    arquivo = fopen("dados.bin", "rb");
+
+    if(arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        exit(1);
+    }
+
+    printf("\n--- Lista de Produtos Cadastrados ---\n");
+    while(fread(&produto, sizeof(Produto), 1, arquivo)) {
+        printf("ID: %d\n", produto.id);
+        printf("Nome: %s\n", produto.nome);
+        printf("Quantidade: %d\n", produto.quantidade);
+        printf("Preco: %.2lf\n", produto.preco);
+        printf("-----------------------------------\n");
+    }
+    fclose(arquivo);
+}
+
+void editarProdutos()
+{
+    FILE *arquivo;
+    Produto produto;
+    int id_busca, encontrou = 0;
+
+    arquivo = fopen("dados.bin", "rb+");
+    if(arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        exit(1);
+    }
+
+    printf("\nDigite o ID para pesquisa: ");
+    scanf("%d", &id_busca);
+    getchar();
+    while(fread(&produto, sizeof(Produto), 1, arquivo)) {
+        if(produto.id == id_busca) {
+            encontrou = 1;
+
+            printf("\n--- Registro Encontrado ---\n");
+            printf("ID: %d\n", produto.id);
+            printf("Nome: %s\n", produto.nome);
+            printf("Quantidade: %d\n", produto.quantidade);
+            printf("Preco: %.2lf\n", produto.preco);
+            printf("---------------------------\n");
+
+            printf("\nDigite o novo nome: ");
+            fgets(produto.nome, sizeof(produto.nome), stdin);
+            produto.nome[strcspn(produto.nome, "\n")] = '\0';
+
+            printf("Digite a nova quantidade: ");
+            scanf("%d", &produto.quantidade);
+
+            printf("Digite o novo preco: ");
+            scanf("%lf", &produto.preco);
+
+            fseek(arquivo, -(long)sizeof(Produto), SEEK_CUR);
+            fwrite(&produto, sizeof(Produto), 1, arquivo);
+
+            printf("\n--- Registro alterado com sucesso ---\n");
+            printf("ID: %d\n", produto.id);
+            printf("Nome: %s\n", produto.nome);
+            printf("Quantidade: %d\n", produto.quantidade);
+            printf("Preco: %.2lf\n", produto.preco);
+            printf("-------------------------------------\n");
+            break;
+        }
+    }
+    fclose(arquivo);
+
+    if(encontrou == 0) {
+        printf("Nao ha registro com o valor pesquisado.\n");
+    }
+
+}
+
+void excluirProdutos() {
+    FILE *arquivo, *novoArquivo;
+    Produto produto;
+    int id_busca, op, encontrou = 0, exclui = 0;
+
+    arquivo = fopen("dados.bin", "rb");
+    if(arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        exit(1);
+    }
+
+    novoArquivo = fopen("temp.bin", "wb");
+    if(novoArquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        fclose(arquivo);
+        exit(1);
+    }
+
+    printf("\nDigite o ID do registro para exclusao: ");
+    scanf("%d", &id_busca);
+    getchar();
+
+    while(fread(&produto, sizeof(Produto), 1, arquivo)) {
+        if(produto.id == id_busca) {
+            encontrou = 1;
+            printf("\n--- Registro Encontrado ---\n");
+            printf("ID: %d\n", produto.id);
+            printf("Nome: %s\n", produto.nome);
+            printf("Quantidade: %d\n", produto.quantidade);
+            printf("Preco: %.2lf\n", produto.preco);
+            printf("---------------------------\n");
+
+            printf("Deseja realmente excluir este registro? (1- Sim | 2- Nao): ");
+            scanf("%d", &op);
+            getchar();
+
+            if(op != 1) {
+                fwrite(&produto, sizeof(Produto), 1, novoArquivo);
+            }
+        } else {
+            fwrite(&produto, sizeof(Produto), 1, novoArquivo);
+        }
+    }
+
+    fclose(arquivo);
+    fclose(novoArquivo);
+
+    if(encontrou == 0) {
+        printf("\nNao ha registro com o valor pesquisado.\n");
+        remove("temp.bin");
+    } else if(op == 1 && encontrou == 1) {
+        if(remove("dados.bin")!= 0) {
+            perror("Erro ao remover o arquivo original");
+            exit(1);
+        }
+
+        if(rename("temp.bin", "dados.bin") != 0) {
+            perror("Erro ao renomear o arquivo");
+            exit(1);
+        }
+
+        printf("\n--- Registro excluido com sucesso ---\n");
+    } else {
+        remove("temp.bin");
     }
 }
 
 int main()
 {
-    cadastrarProdutos();
+    int opcao;
+
+    do {
+        printf("\n--- Sistema Hortifruti ---\n");
+        printf("1. Cadastrar Produto\n");
+        printf("2. Consultar Produto por ID\n");
+        printf("3. Consultar Todos os Produtos\n");
+        printf("4. Editar Produto\n");
+        printf("5. Excluir Produto\n");
+        printf("6. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        getchar();
+
+        switch(opcao) {
+            case 1:
+                cadastrarProdutos();
+                break;
+            case 2:
+                consultarProdutos();
+                break;
+            case 3:
+                consultarTodosProdutos();
+                break;
+            case 4:
+                editarProdutos();
+                break;
+            case 5:
+                excluirProdutos();
+                break;
+            case 6:
+                printf("Saindo do sistema...\n");
+                break;
+            default:
+                printf("Opcao invalida! Tente novamente.\n");
+        }
+    } while(opcao != 6);
+
     return 0;
 }
+
