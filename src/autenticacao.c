384b2823ad6cd596f1f59b304a78da6fb91ba260
matheusdmlopes <matheusdemarcolopes@gmail.com>
2024-11-29 00:51:13 -0300

    Adição do nome de usuario obrigatorio pra acesso ao sistema

--- src/autenticacao.c
+++ src/autenticacao.c
@@ -2,17 +2,22 @@
 #include <string.h>
 #include <autenticacao.h>
 
+#define USUARIO "Rogerio"
 #define SENHA "UNIP"
 
 int autenticarUsuario() {
-    char senha[5], senhaDigitada[20];
+    char senhaDigitada[20], usuarioDigitado[20];
 
-    strcpy(senha, "UNIP");
 
-    printf("Por favor, insira a senha para acessar o sistema.\n");
+    printf("Por favor, insira o usuário e senha para acessar o sistema.\n");
     printf("Você pode digitar 'sair' para encerrar o programa.\n");
 
     do {
+        printf("Digite o usuario: ");
+        fgets(usuarioDigitado, sizeof(usuarioDigitado), stdin);
+
+        usuarioDigitado[strcspn(usuarioDigitado, "\n")] = '\0';
+
         printf("Digite a senha: ");
         fgets(senhaDigitada, sizeof(senhaDigitada), stdin);
 
@@ -23,12 +28,17 @@ int autenticarUsuario() {
 
         senhaDigitada[strcspn(senhaDigitada, "\n")] = '\0';
 
+        if (strcmp(usuarioDigitado, "sair") == 0 || strcmp(usuarioDigitado, "SAIR") == 0) {
+            printf("\nEncerrando o sistema... Até mais!\n");
+            return 0;
+        }
+
         if (strcmp(senhaDigitada, "sair") == 0 || strcmp(senhaDigitada, "SAIR") == 0) {
             printf("\nEncerrando o sistema... Até mais!\n");
             return 0;
         }
 
-        if (strlen(senhaDigitada) != strlen(senha) || strcmp(senha, senhaDigitada) != 0) {
+        if (strcmp(USUARIO, usuarioDigitado) != 0 || strcmp(SENHA, senhaDigitada) != 0) {
             printf("\nAcesso negado. Tente novamente ou digite 'sair' para encerrar.\n");
         } else {
             printf("\nAcesso autorizado.\n");
